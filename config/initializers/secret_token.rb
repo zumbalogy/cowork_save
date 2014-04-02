746252da2cc48c5825e9b9dc5438771108e23318
zumbalogy <intrapostal@gmail.com>
2014-04-02 12:47:42 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jagg::Application.config.secret_key_base = 'f55a421afc645b94752ce712312801228dd4656a8d4050e24499b23180edf5535b7ebfd6ae176c09175af07c5eb3e37e8f10ebd8a3898691357d107dd52ae4f9'
